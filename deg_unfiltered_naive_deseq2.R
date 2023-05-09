90d2cb975a92f39250fa82e2c5551b550a2d2063
laupierre <laupierre@users.noreply.github.com>
2023-05-09 11:50:54 +0200

    Update deg_unfiltered_naive_deseq2.R

--- deg_unfiltered_naive_deseq2.R
+++ deg_unfiltered_naive_deseq2.R
@@ -1,4 +1,4 @@
-## 
+## 210825_A00558_0141_AH232YDMXY
 
 library (openxlsx)
 library (DESeq2)
@@ -22,6 +22,19 @@ a <- a[ ,grep ("gene_type.y|gene_name.y", colnames (a), invert=TRUE)]
 colnames (a) [colnames (a) == "gene_name.x"] <- "gene_name"
 colnames (a) [colnames (a) == "gene_type.x"] <- "gene_type"
 
+colnames (a) <- gsub ("star.IIT_", "", colnames (a))
 #write.xlsx (a, "star_gene_raw_counts.xlsx", rowNames=F)
+
+
+
+## Naive experiment
+
+#FR: 1623, 1636, 1637, 1638, 1971, 1972, 1973
+#ST: 1627, 1628, 1629, 1631, 1962, 1966, 1950
+#OR: 1616, 1617, 1618, 1622, 1947, 1949, 1952
+
+pheno <- data.frame (sample=c("RNT_1623", "RNT_1636", "RNT_1637", "RNT_1638", "RNT_1971", "RNT_1972", "RNT_1973",
+            "RNT_1627", "RNT_1628", "RNT_1629", "RNT_1631", "RNT_1962", "RNT_1966", "RNT_1950",
+            "RNT_1616", "RNT_1617", "RNT_1618", "RNT_1622", "RNT_1947", "RNT_1949", "RNT_1952"))
 
 
