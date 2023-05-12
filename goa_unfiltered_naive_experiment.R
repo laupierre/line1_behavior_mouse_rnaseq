1391881e3a88707de2c32714dc648b3366cc231c
laupierre <laupierre@users.noreply.github.com>
2023-05-12 15:15:03 +0200

    Update goa_unfiltered_naive_experiment.R

--- goa_unfiltered_naive_experiment.R
+++ goa_unfiltered_naive_experiment.R
@@ -2,13 +2,16 @@ library (gprofiler2)
 library (openxlsx)
 
 deg <- read.xlsx ("striatum_deseq2_tespex_OTvsFR_differential_expression.xlsx")
+custom_bg <- deg$gene_name
+
 deg <- deg [deg$padj < 0.05, ]
 deg_up <- deg[deg$log2FoldChange > 0, ]
 deg_up <- deg_up$gene_name
 head (deg_up)
 
 
-gp_up <- gost(query = deg_up, organism = "mmusculus", numeric_ns = "ENTREZGENE_ACC", sources = c("GO:BP"), exclude_iea=TRUE, evcodes = TRUE, significant = FALSE)
+gp_up <- gost(query = deg_up, organism = "mmusculus", numeric_ns = "ENTREZGENE_ACC", sources = c("GO:BP"), exclude_iea=TRUE, evcodes = TRUE, significant = TRUE,
+              custom_bg = custom_bg, domain_scope = "custom_annotated")
 gp_up <- gp_up$result[ ,c("query", "significant", "p_value", "term_name", "term_size", "intersection_size", "intersection")]
 gp_up$query <- "up"
 head(gp_up)
