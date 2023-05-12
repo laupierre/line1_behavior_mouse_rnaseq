c6d7fc09201f97437cd81c525e8475bcbdefbdc8
laupierre <laupierre@users.noreply.github.com>
2023-05-12 15:48:00 +0200

    Update goa_unfiltered_naive_experiment.R

--- goa_unfiltered_naive_experiment.R
+++ goa_unfiltered_naive_experiment.R
@@ -3,6 +3,9 @@
 library (gprofiler2)
 library (openxlsx)
 
+
+## first contrast
+
 deg <- read.xlsx ("striatum_deseq2_tespex_OTvsFR_differential_expression.xlsx")
 custom_bg <- deg$gene_name
 
@@ -17,6 +20,35 @@ gp_up <- gp_up$result[ ,c("term_id", "term_name", "significant", "p_value", "ter
 write.xlsx (gp_up, "striatum_deseq2_tespex_OTvsFR_gene_ontology_analysis_up_genes.xlsx")
 
 
+deg_down <- deg[deg$log2FoldChange < 0, ]
+deg_down <- deg_down$gene_name
+head (deg_down)
+
+gp_down <- gost(query = deg_down, organism = "mmusculus", numeric_ns = "ENTREZGENE_ACC", sources = c("GO:BP"), exclude_iea=TRUE, evcodes = TRUE, significant = TRUE,
+              custom_bg = custom_bg, domain_scope = "custom_annotated")
+gp_down <- gp_down$result[ ,c("term_id", "term_name", "significant", "p_value", "term_size", "query_size", "intersection_size", "parents", "intersection")]
+write.xlsx (gp_down, "striatum_deseq2_tespex_OTvsFR_gene_ontology_analysis_down_genes.xlsx")
+
+
+
+
+## second contrast
+
+rm (deg)
+
+deg <- read.xlsx ("striatum_deseq2_STvsFR_differential_expression.xlsx")
+custom_bg <- deg$gene_name
+
+deg <- deg [deg$padj < 0.05, ]
+deg_up <- deg[deg$log2FoldChange > 0, ]
+deg_up <- deg_up$gene_name
+head (deg_up)
+
+gp_up <- gost(query = deg_up, organism = "mmusculus", numeric_ns = "ENTREZGENE_ACC", sources = c("GO:BP"), exclude_iea=TRUE, evcodes = TRUE, significant = TRUE,
+              custom_bg = custom_bg, domain_scope = "custom_annotated")
+gp_up <- gp_up$result[ ,c("term_id", "term_name", "significant", "p_value", "term_size", "query_size", "intersection_size", "parents", "intersection")]
+write.xlsx (gp_up, "striatum_deseq2_tespex_STvsFR_gene_ontology_analysis_up_genes.xlsx")
+
 
 deg_down <- deg[deg$log2FoldChange < 0, ]
 deg_down <- deg_down$gene_name
@@ -26,5 +58,13 @@ gp_down <- gost(query = deg_down, organism = "mmusculus", numeric_ns = "ENTREZGE
               custom_bg = custom_bg, domain_scope = "custom_annotated")
 gp_down <- gp_down$result[ ,c("term_id", "term_name", "significant", "p_value", "term_size", "query_size", "intersection_size", "parents", "intersection")]
 write.xlsx (gp_down, "striatum_deseq2_tespex_OTvsFR_gene_ontology_analysis_down_genes.xlsx")
+
+
+
+
+
+
+
+
 
 
